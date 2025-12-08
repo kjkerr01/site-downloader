92394e004d4eb37f70812bca36b69bc0a0d97df0
kjkerr01 <kainoa1kerr@gmail.com>
2025-12-08 12:08:37 -0700

    Update app.py

--- app.py
+++ app.py
@@ -113,6 +113,6 @@ def download_site():
             pass
 
 if __name__ == "__main__":
-import os
-port = int(os.environ.get("PORT", 8080))
-app.run(host="0.0.0.0", port=port)
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
